0e8ae9dc55dfb2ae7df6329c460932ab9a75b773
Isadora-Kairala <isadorarequer7191@gmail.com>
2026-04-27 09:38:56 -0300

    feat: atualização na Classe registrável
            Co-authored-by: Migdmo <Miguelmoura112@gmail.com>

--- src/Registravel.java
+++ src/Registravel.java
@@ -2,4 +2,12 @@ public interface Registravel {
 
     public void registrar();
 
+    //commit pair extraodinaire
+    /*
+
+        git commit -m "feat: atualização na Classe registrável
+
+        Co-authored-by: Migdmo <Miguelmoura112@gmail.com>"
+
+     */
 }
